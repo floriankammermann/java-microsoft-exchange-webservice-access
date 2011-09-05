e1a34ae76e990a4bc5133dd1bee1633580f8ba63
Florian Kammermann <florian.kammermann@gmail.com>
2011-09-05 15:17:21 +0200

    move package

---
+++ ews-api/src/test/java/com/microsoft/TestExchangeWebservice.java
@@ -0,0 +1,47 @@
+package com.microsoft;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.junit.Test;
+
+import com.microsoft.schemas.exchange.services._2006.messages.ExchangeService;
+
+
+public class TestExchangeWebservice {
+
+	@Test
+	public void testGetMail() throws MalformedURLException {
+		
+		 // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            public void checkClientTrusted(
+                    java.security.cert.X509Certificate[] certs, String authType) {
+            }
+
+            public void checkServerTrusted(
+                    java.security.cert.X509Certificate[] certs, String authType) {
+            }
+        } };
+
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+        }
+	
+		//ExchangeService esw = new ExchangeService(new URL("https://webmail.evd.admin.ch/ews/exchange.asmx?wsdl"));
+		ExchangeService esw = new ExchangeService();
+		
+	}
+}
